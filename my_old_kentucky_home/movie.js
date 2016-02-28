a3963bd07627d0b1fa9ddf24272b1c96259d47ed
Adam Plumer <caerus.karu@gmail.com>
2016-02-28 02:43:40 -0500

    Final commit, all details implemented correctly

--- my_old_kentucky_home/movie.js
+++ my_old_kentucky_home/movie.js
@@ -1,18 +1,22 @@
-song = ["♪ ♪ ♪ My Old Kentucky Home ♪ ♪ ♪",
-	"The sun shines bright in My Old Kentucky Home,",
-	"'Tis summer, the people are gay;",
-	"The corn-top's ripe and the meadow's in the bloom",
-	"While the birds make music all the day.",
-	"The young folks roll on the little cabin floor,",
-	"All merry, all happy and bright;",
-	"By 'n' by hard times comes a knocking at the door,",
-	"Then My Old Kentucky Home, good night!",
-	"Weep no more my lady",
-	"Oh weep no more today;",
-	"We will sing one song",
-	"For My Old Kentucky Home",
-	"For My Old Kentucky Home, far away"
-       ];
+var song = ["♪ ♪ ♪ My Old Kentucky Home ♪ ♪ ♪",
+	    "The sun shines bright in My Old Kentucky Home,",
+	    "'Tis summer, the people are gay;",
+	    "The corn-top's ripe and the meadow's in the bloom",
+	    "While the birds make music all the day.",
+	    "The young folks roll on the little cabin floor,",
+	    "All merry, all happy and bright;",
+	    "By 'n' by hard times comes a knocking at the door,",
+	    "Then My Old Kentucky Home, good night!",
+	    "Weep no more my lady",
+	    "Oh weep no more today;",
+	    "We will sing one song",
+	    "For My Old Kentucky Home",
+	    "For My Old Kentucky Home, far away"
+	   ];
+
+var imgpath = "images/";
+var img1 = imgpath + "sunshine.jpg";
+var img2 = imgpath + "corn.jpg";
 
 var el = document.getElementById("music_video");
 
@@ -21,47 +25,57 @@ el.addEventListener("timeupdate", function() {
     var pb = "<p>";
     var ps = "</p>";
     var lyric = document.getElementById("lyrics");
+
+    lyric.innerHTML = pb;
+
     if (time >= 0 && time < 9) {
-	lyric.innerHTML = pb + song[0] + ps;
+	lyric.innerHTML += song[0];
     }
     else if (time >= 9 && time < 15) {
-	lyric.innerHTML = pb + song[1] + ps;
+	lyric.innerHTML += song[1];
+	lyric.innerHTML += ps + pb;
+	lyric.innerHTML += "<img src=" + img1 + ' alt="sun shines" height="99" width="200">';
+	lyric.innerHTML += ps;
     }
     else if (time >= 15 && time < 22) {
-	lyric.innerHTML = pb + song[2] + ps;
+	lyric.innerHTML += song[2];
     }
     else if (time >= 22 && time < 28) {
-	lyric.innerHTML = pb + song[3] + ps;
+	lyric.innerHTML += song[3];
+	lyric.innerHTML += ps + pb;
+	lyric.innerHTML += "<img src=" + img2 + ' alt="corn tops" height="79" width="200">';
+	lyric.innerHTML += ps;
     }
     else if (time >= 28 && time < 36) {
-	lyric.innerHTML = pb + song[4] + ps;
+	lyric.innerHTML += song[4];
     }
     else if (time >= 36 && time < 43) {
-	lyric.innerHTML = pb + song[5] + ps;
+	lyric.innerHTML += song[5];
     }
     else if (time >= 43 && time < 49) {
-	lyric.innerHTML = pb + song[6] + ps;
+	lyric.innerHTML += song[6];
     }
     else if (time >= 49 && time < 56) {
-	lyric.innerHTML = pb + song[7] + ps;
+	lyric.innerHTML += song[7];
     }
     else if (time >= 56 && time < 64) {
-	lyric.innerHMTL = pb + song[8] + ps;
+	lyric.innerHTML += song[8];
     }
     else if (time >= 64 && time < 70) {
-	lyric.innerHTML = pb + song[9] + ps;
+	lyric.innerHTML += song[9];
     }
     else if (time >= 70 && time < 77) {
-	lyric.innerHTML = pb + song[10] + ps;
+	lyric.innerHTML += song[10];
     }
     else if (time >= 77 && time < 80) {
-	lyric.innerHTML = pb + song[11] + ps;
+	lyric.innerHTML += song[11];
     }
     else if (time >= 80 && time < 84) {
-	lyric.innerHTML = pb + song[12] + ps;
+	lyric.innerHTML += song[12];
     }
     else if (time >= 84) {
-	lyric.innerHTML = pb + song[13] + ps;
+	lyric.innerHTML += song[13];
     }
+    lyric.innerHTML += ps;
 }, false);
 
