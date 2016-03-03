bc349a1a7ed5b19c9e1b2a0b58fa7e28cd6f44ac
Adam Plumer <caerus.karu@gmail.com>
2016-03-03 13:16:27 -0500

    Added spanning and actually applied the css styles correctly

--- messages/lab.js
+++ messages/lab.js
@@ -4,6 +4,9 @@ var file = "data.json";
 var use_resource = file;
 var header = "messages";
 var http = new XMLHttpRequest();
+var hs = '<span class="message">';
+var he = '</span>';
+var br = '<br /><br />';
 
 function parse() {
 
@@ -14,7 +17,7 @@ function parse() {
 	    var parse = JSON.parse(http.responseText);
 	    var el = document.getElementById(header);
 	    for (var i = 0; i < parse.length; i++) {
-		el.innerHTML += "<p>" + parse[i].content + " " + parse[i].username + "</p>";
+		el.innerHTML += hs + parse[i].content + " " + parse[i].username + he + br;
 	    }
 	}
     });
